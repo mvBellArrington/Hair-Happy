e785d45739e90615be1843dc0e39be8f191e5d45
Mone Bell-Arrington <mvbellarrington@my.waketech.edu>
2021-10-02 01:05:38 -0400

    Added Data Classes

---
+++ Hair Happy App/Hair Happy App/Data/ServiceDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hair_Happy_App.Models;
+
+namespace Hair_Happy_App.Data
+{
+    class ServiceDatabase
+    {
+    }
+}
--- Hair Happy App/Hair Happy App/Models/Service.cs
+++ Hair Happy App/Hair Happy App/Models/Service.cs
@@ -6,5 +6,11 @@ namespace Hair_Happy_App.Models
 {
     class Service
     {
+        public long ServiceId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Cost { get; set; }
+        public DateTime TimeSpent { get; set; }
+
     }
 }
